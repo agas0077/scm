76e9267c9139878d184d1bf71fc7022cf3cb55b2
Andrei Agasiants <Andrei.Agasiants@unilever-rus.ru>
2023-06-07 17:12:15 +0300

    Fix mentor btn and mobile menu. Move fouders block

---
+++ scm/static/assets/js/about_us_mobile.js
@@ -0,0 +1,5 @@
+$('#about-us-mobile').click(() => {
+    $('#navbar').removeClass('navbar-mobile')
+    $('.mobile-nav-toggle').removeClass('bi-x')
+    $('.mobile-nav-toggle').addClass('bi-list')
+})
--- scm/static/assets/js/highlight_mentor.js
+++ scm/static/assets/js/highlight_mentor.js
@@ -1,14 +1,36 @@
 $('#mentor-why-us').click(() => {
-    const mentorDropdown = $('#header-mentor-dropdown')
+    const mentorDropdown = $('#header-mentor-dropdown');
+    const mobileMenu = $('.mobile-nav-toggle');
+
     if (mentorDropdown.html() != 'Заявка уже подана') {
-        mentorDropdown.addClass('get-started-btn');
-        mentorDropdown.animate({ rotate: '40deg' }, 150)
-        mentorDropdown.animate({ rotate: '-40deg' }, 150)
-        mentorDropdown.animate({ rotate: '0deg' }, 150)
-        window.setInterval(function () {
-            mentorDropdown.animate({ rotate: '40deg' }, 200)
-            mentorDropdown.animate({ rotate: '-40deg' }, 200)
-            mentorDropdown.animate({ rotate: '0deg' }, 200)
-        }, 5000);
+        if ($(window).width() < 991) {
+            mobileMenu.addClass('mobile-menu-btn');
+
+            const intervalMenu = window.setInterval(function () {
+                mobileMenu.animate({ rotate: '50deg' }, 300);
+                mobileMenu.animate({ rotate: '-50deg' }, 300);
+                mobileMenu.animate({ rotate: '0deg' }, 300);
+            }, 2500);
+
+            mobileMenu.click(() => {
+                mobileMenu.removeClass('mobile-menu-btn');
+                window.clearInterval(intervalMenu);
+            });
+
+        } else {
+
+            mentorDropdown.addClass('mentor-btn');
+
+            const intervalDropdown = window.setInterval(function () {
+                mentorDropdown.animate({ rotate: '20deg' }, 300);
+                mentorDropdown.animate({ rotate: '-20deg' }, 300);
+                mentorDropdown.animate({ rotate: '0deg' }, 300);
+            }, 2500);
+
+            mentorDropdown.hover(() => {
+                mentorDropdown.removeClass('mentor-btn');
+                window.clearInterval(intervalDropdown);
+            });
+        }
     }
 });
